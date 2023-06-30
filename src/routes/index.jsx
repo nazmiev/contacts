34776d077d6a26f0132d538daf65c9cb8b1f1ad9
nazmiev <roma.nazmiev@gmail.com>
2023-06-30 12:18:02 +0700

    first commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <p id="zero-state">
+      Выберите контакт слева
+      <br />
+      или напишите <a href="https://t.me/roma_nazmiev">Роману</a>.
+    </p>
+  );
+}
